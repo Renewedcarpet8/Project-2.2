2511497837f1d455b99de89c7a970147af9f35cc
doomedxx <wesley.dw@live.nl>
2018-01-30 00:43:46 +0100

    Sidebar, Bottombar;

--- Applicatie/index.php
+++ Applicatie/index.php
@@ -1,8 +1,10 @@
 
 <html>
     <head>
-        <link rel="stylesheet" href="style.css" type="text/css"/>
-
+        <link rel="stylesheet" href="css/sidenav.css" type="text/css"/>
+        <link rel="stylesheet" href="css/main.css" type="text/css"/>
+        <link rel="stylesheet" href="css/topnav.css" type="text/css"/>
+        <script type="text/javascript" src="js/menumaker.js"></script>
         <title>Weather Pressure Application</title>
         <meta charset="UTF-8">
         <link rel="icon" 
@@ -11,27 +13,44 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <img class="schoollogo" src="img/schoollogo.png">
+<!-- Side navigation -->
+<div class="sidenav">
+  <a href="#">OPTIE 1</a>
+  <a href="#">OPTIE 2</a>
+  <a href="#">OPTIE 3</a>
+  <a href="#">BLA BLA BLA  </a>
+  <img class="SettingsIcon" src="img/settings.png"></a>
+</div>
+
+<div class="topnav">
+  <li><a href="#" class="t" >WEERGAVE 1</a></li>
+  <li><a href="#" class="t" >WEERGAVE 2</a></li>
+  <li><a href="#" class="t" >WEERGAVE 3</a></li>
+  <li><a href="#" class="t" >WEERGAVE 4</a></li>
+</div>
+
+<!-- Page content -->
+<div id="main">
+  ...
+</div>
+
+<div class="container"><div class="menu-icon"><span></span></div></div>
+        
         <div class="dashboard" method="POST" action="login-process.php">
-          <H1>Welcome</H1>
+          <H1>Bla bla bla</H1>
           <p class="login-text">
             <span class="fa-stack fa-lg">
               <i class="fa fa-circle fa-stack-2x"></i>
               <i class="fa fa-lock fa-stack-1x"></i>
             </span>
           </p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sollicitudin tortor faucibus risus mollis, id facilisis lacus pulvinar. Aliquam ut tincidunt mi, vitae tempor quam. Vivamus dapibus sagittis nisi, vitae tempor odio efficitur id. Nulla rutrum facilisis congue. In placerat sem turpis. Fusce a nisl vel eros tincidunt placerat a quis quam. Phasellus ac varius ex. Ut a mi aliquam, euismod elit a, bibendum sem. Vestibulum porttitor nisi ac massa interdum porttitor. Vestibulum quis felis mollis, sagittis diam quis, imperdiet ante.</p>
-
-          <p>Morbi ac turpis quis velit condimentum sagittis et blandit felis. Vivamus sem felis, fringilla sed lacus ut, pellentesque vulputate leo. In hac habitasse platea dictumst. In ultricies cursus felis. Phasellus congue rutrum velit sed volutpat. Nulla euismod nulla quis quam feugiat, et tempor ante congue. Aliquam faucibus vitae massa ac condimentum. Donec imperdiet aliquam lacus non volutpat.</p>
-
-          <p>In hac habitasse platea dictumst. Nulla non ex tempor, laoreet lectus a, convallis leo. Vestibulum ornare et elit at porttitor. Nunc placerat lobortis arcu non dictum. Morbi eleifend, odio eget rhoncus accumsan, erat arcu elementum justo, vestibulum luctus arcu mi ut massa. Etiam et sagittis massa, eget vehicula enim. Vestibulum eget turpis ac magna viverra finibus et in ex. Fusce gravida sapien sed nisi mollis efficitur.</p>
-
-          <p>ed et lacinia orci, id porttitor ipsum. Suspendisse consequat rhoncus nisi aliquet venenatis. In id eros id massa fringilla tempor. Suspendisse venenatis molestie mi venenatis dignissim. Sed pharetra congue finibus. Fusce sem turpis, pretium vitae mollis et, viverra sed urna. Pellentesque eu vestibulum ante, eu imperdiet dolor.</p>
+          <img src="img/placeholdergraph.png">
         </div>
         <div class="underlay-photo"></div>
         <div class="underlay-black"></div> 
         <img class="hanzelogo" src="img/hanzelogo.png">
     </body>
-      
-    </body>
+      <script type="text/javascript">
+	$("#cssmenu").menumaker();
+</script>
 </html>
