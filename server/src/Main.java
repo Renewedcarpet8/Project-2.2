3d1afd15d9d9946667846841144e2aaec7e23d2b
jhhaanstra <janhendrikhaanstra@gmail.com>
2018-01-29 12:32:01 +0100

    fixed flags bug and updated measurements

--- server/src/Main.java
+++ server/src/Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
         DataLog log = new DataLog();
         log.readLog();
-        log.writeToLog("test123");
         log.readLog();
     }
 }
