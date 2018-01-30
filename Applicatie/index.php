b3ae1e7fe9d89d6eaae666d19df50e295edebcfc
doomedxx <wesley.dw@live.nl>
2018-01-30 15:50:39 +0100

    2 containers - Data Values en Bediening

--- Applicatie/index.php
+++ Applicatie/index.php
@@ -13,58 +13,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-    <?php
-       /* if (!isset($_POST['Login'])) {
-          echo "Please login first<br>";
-          echo "<a href='login.php'>Click here to log in<a>";
-        } else {*/
-        function checkUser(){
-            if (isset($_POST['Login'])) {
-            $username = trim($_POST['username']);
-            $password = trim($_POST['password']);
-
-          $csv = array();
-
-          if ($handle = fopen("users.csv", "r")) {
-            while (($data = fgetcsv($handle, 1000, ",")) !==FALSE) {
-              $csv[] = $data;
-            }
-          }
-        
-          $succes = false;
-          $usersucces = false;
-
-          $j = 0;
-          while ($j < count($csv)) {
-            if ($username == $csv[$j][1] && $password == $csv[$j][2]) {
-              $succes = true;
-              return true;
-            } elseif ($username == $csv[$j][1] && $password !== $csv[$j][2]) {
-              $usersucces = true;
-            }
-            $j++;
-          }
-          
-          fclose($handle);
-            }
-        }
-        ?>
-         <div class="sidenav">
-           <?php if(checkUser() == true) {
-            echo "<a href='#'>OPTIE 1</a>";
-            echo "<a href='#'>OPTIE 2</a>";
-            echo "<a href='#'>OPTIE 3</a>";
-            echo "<a href='#'>BLA BLA BLA  </a>";} ?>
+        <div class="sidenav">
+            <a href='#'>OPTIE 1</a>";
+            <a href='#'>OPTIE 2</a>";
+            <a href='#'>OPTIE 3</a>";
+            <a href='#'>BLA BLA BLA  </a>
             <img class="SettingsIcon" src="img/settings.png"></a>
 
           </div>
 
           <div class="topnav">
-              <?php if (checkUser() == true) {
-                echo "<li><a href='#' class='t' >WEERGAVE 1</a></li>";
-                echo "<li><a href='#' class='t' >WEERGAVE 1</a></li>";
-                echo "<li><a href='#' class='t' >WEERGAVE 1</a></li>";
-                echo "<li><a href='#' class='t' >WEERGAVE 1</a></li>";}?>
+                <li><a href='#' class='t' >WEERGAVE 1</a></li>
+                <li><a href='#' class='t' >WEERGAVE 1</a></li>
+                <li><a href='#' class='t' >WEERGAVE 1</a></li>
+                <li><a href='#' class='t' >WEERGAVE 1</a></li>
           </div>
 
           <!-- Page content -->
@@ -72,34 +34,41 @@
             ...
           </div>
 
-          <div class="container"><div class="menu-icon"><span></span></div></div>
+          <div class="container-graph"><div class="menu-icon"><span></span></div></div>
                   
-                  <div class="dashboard" method="POST" action="login-process.php">
-                      <?php
-                      if (checkUser() == true) {
-                        echo "<H1>Bla bla bla</br> </br></H1>";
-                        echo "<span class='fa-stack fa-lg'>";
-                            echo "<i class='fa fa=circle fa-stack-2x'></i>";
-                            echo "<i class='fa fa-lock fa-stack-1x\'></i>";
-                        echo "</span>";
-                        echo "</p>";
-                        echo "<img class='graphplaceholder' src='img/placeholdergraph.png'";
-                      } else {
-                          if (!isset($_POST['Login'])){
-                              echo "<h1>Please login first";
-                              echo "<a class='login-warning' href='login.php'>Click here to log in</a></h1>";
-                          } else {
-                              echo "<h1> login failed</h1>";
-                          }
-                      }
-
-                      ?>
+                  <div class="dashboard-graph" method="POST">
+                        <H1>GRAFIEK HIER</br> </br></H1>
+                        <span class='fa-stack fa-lg'>
+                            <i class='fa fa=circle fa-stack-2x'></i>
+                            <i class='fa fa-lock fa-stack-1x'</i>
+                        </span>
+                        </p>
+                        <img class='graphplaceholder' src='img/placeholdergraph.png'>
 
 
-                  </div>
-                  <div class="underlay-photo"></div>
-                  <div class="underlay-black"></div>
-                  <img class="hanzelogo" src="img/schoollogo-small.png">
+          </div>
+          <div class ="container-control">
+                  <div class="dashboard-control" method="POST">
+                        <H1>EXTRA BEDIENING <br> HIER</br> </br></H1>
+                        <span class='fa-stack fa-lg'>
+                            <i class='fa fa=circle fa-stack-2x'></i>
+                            <i class='fa fa-lock fa-stack-1x'</i>
+                        </span>
+                        </p>
+        </div>
+          <div class ="container-values">
+                  <div class="dashboard-values" method="POST">
+                        <H1>DATA VALUES HIER</br> </br></H1>
+                        <span class='fa-stack fa-lg'>
+                            <i class='fa fa=circle fa-stack-2x'></i>
+                            <i class='fa fa-lock fa-stack-1x'</i>
+                        </span>
+                        </p>
+        </div>
+              
+          <div class="underlay-photo"></div>
+          <div class="underlay-black"></div>
+          <img class="schoollogographmode" src="img/schoollogo-small.png">
               </body>
                 <script type="text/javascript">
             $("#cssmenu").menumaker();
