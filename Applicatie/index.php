ee073a8a9ede93081aa876a9884fb9febda3aa75
doomedxx <wesley.dw@live.nl>
2018-02-07 00:19:19 +0100

    De magische </p> herstelt waardoor de login screen weer goed is. + Conflict fix

--- Applicatie/index.php
+++ Applicatie/index.php
@@ -1,19 +1,19 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <link rel="stylesheet" href="./css/main.css" type="text/css"/>
-	<link rel="stylesheet" href="./css/graphview.css" type="text/css"/>
-	<link rel="stylesheet" href="./css/sidenav.css" type="text/css"/>
-	<link rel="stylesheet" href="./css/topnav.css" type="text/css"/>
-    <title>Weather Pressure Application</title>
-    <meta charset="UTF-8">
-    <link rel="icon"
-          type="image/png"
-          href="img/tab_icon.png">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-    <?php
+    <head>
+        <link rel="stylesheet" href="./css/main.css" type="text/css"/>
+        <link rel="stylesheet" href="./css/graphview.css" type="text/css"/>
+        <link rel="stylesheet" href="./css/sidenav.css" type="text/css"/>
+        <link rel="stylesheet" href="./css/topnav.css" type="text/css"/>
+        <title>Weather Pressure Application</title>
+        <meta charset="UTF-8">
+        <link rel="icon"
+              type="image/png"
+              href="img/tab_icon.png">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <?php
         session_start();
         //require 'errorstack.php';
         $csv = array();
@@ -37,15 +37,15 @@
                     $userNumber = $csv[$j];
                     $_SESSION['usernr'] = $userNumber;
                     $_SESSION['username'] = $username;
-                } elseif ($username == $csv[$j][1] && $password !== $csv[$j][2]){
+                } elseif ($username == $csv[$j][1] && $password !== $csv[$j][2]) {
                     $usersucces = true;
                 }
                 $j++;
             }
 
-            if ($succes){
+            if ($succes) {
                 $_SESSION['errormsg'] = "";
-            } elseif ($usersucces){
+            } elseif ($usersucces) {
                 $_SESSION['errormsg'] = "Wrong password";
             } else {
                 $_SESSION['errormsg'] = "Unknown user";
@@ -53,44 +53,44 @@
 
             fclose($handle);
         }
-                if ($succes) {
-                    header("refresh:0;url=query1.php");
-
-                } else {
-                    if (isset($_SESSION['username'])) {
-                        echo "Welkom";
-                        session_destroy();
-                        header("refresh:0;url=query1.php");
-                    } else {
-
-                        ?>
-                        <img class="schoollogo" src="img/schoollogo.png">
-                        <form class="login-form" method="POST" action="index.php">
-                            <center><img src="img/lockicon.png"></center>
-                            <p class="login-text">
-                            <span class="fa-stack fa-lg">
-                              <i class="fa fa-circle fa-stack-2x"></i>
-                              <i class="fa fa-lock fa-stack-1x"></i>
-                            </span>
-                                <?php
-                                    if (!empty($_SESSION['errormsg'])){
-                                        print_r($_SESSION['errormsg']);
-                                    }
-                                ?>
-                                <input type="UserID" class="login-username" autofocus="true" required="true"
-                                       placeholder="User ID" name="username"/>
-                                <input type="password" class="login-password" required="true" placeholder="Password"
-                                       name="password"/>
-                                <input type="submit" name="Login" value="Login" class="login-submit"/>
-
-                                <a href="mailto:service@ictopia.com" class="login-forgot-pass">forgot password?</a>
-                        </form>
-                    <?php }
+        if ($succes) {
+            header("refresh:0;url=query1.php");
+        } else {
+            if (isset($_SESSION['username'])) {
+                echo "Welkom";
+                session_destroy();
+                header("refresh:0;url=query1.php");
+            } else {
+                ?>
+                <img class="schoollogo" src="img/schoollogo.png">
+                <form class="login-form" method="POST" action="index.php">
+                    <center><img src="img/lockicon.png"></center>
+                    <p class="login-text">
+                        <span class="fa-stack fa-lg">
+                            <i class="fa fa-circle fa-stack-2x"></i>
+                            <i class="fa fa-lock fa-stack-1x"></i>
+                        </span>
+                    </p>
+        <?php
+        if (!empty($_SESSION['errormsg'])) {
+            print_r($_SESSION['errormsg']);
         }
-    ?>
-    <div class="underlay-photo"></div>
-    <div class="underlay-black"></div>
-    <img class="hanzelogo" src="img/hanzelogo.png">
-</body>
+        ?>
+                    <input type="UserID" class="login-username" autofocus="true" required="true"
+                           placeholder="User ID" name="username"/>
+                    <input type="password" class="login-password" required="true" placeholder="Password"
+                           name="password"/>
+                    <input type="submit" name="Login" value="Login" class="login-submit"/>
+
+                    <a href="mailto:service@ictopia.com" class="login-forgot-pass">forgot password?</a>
+                </form>
+    <?php
+    }
+}
+?>
+        <div class="underlay-photo"></div>
+        <div class="underlay-black"></div>
+        <img class="hanzelogo" src="img/hanzelogo.png">
+    </body>
 
 </html>
